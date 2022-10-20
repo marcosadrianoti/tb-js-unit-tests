9df23e7e02efb9cec0bebf68f851bebd2346f76f
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-10-20 14:52:43 -0300

    Inicia o projeto.

--- src/average.js
+++ src/average.js
@@ -3,7 +3,7 @@
   Caso a função receba algum valor não numérico ou um array vazio, o valor undefined deve ser retornado.
   Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
 
-  Parâmetros:
+  Parâmetros: 
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
   Comportamento:
     - average([2, 2]) // Retorno: 2;
