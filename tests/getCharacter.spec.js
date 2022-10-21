36236be1cc4713360ebcea74b7cc94645dda8838
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-10-21 11:12:50 -0300

    Requisito 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,13 +43,25 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
+    const objArya = { name: 'Arya Stark', class: 'Rogue', phrases: ['Not today', 'A girl has no name.'] };
+    const objBrienne = { name: 'Brienne Tarth', class: 'Knight', phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'] };
+    const objMelissandre = { name: 'Melissandre', class: 'Necromancer', phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'] };
+    const aryaNames = ['ARYA', 'Arya', 'aRya', 'arYa', 'aryA', 'arya'];
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(getCharacter()).toBeUndefined();
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    expect(getCharacter('Arya')).toStrictEqual(objArya);
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    expect(getCharacter('Brienne')).toStrictEqual(objBrienne);
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    expect(getCharacter('Melissandre')).toStrictEqual(objMelissandre);
     // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
+    for (const aryaName of aryaNames) {
+      expect(getCharacter(aryaName)).toStrictEqual(objArya);
+    }
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('maria')).toBeUndefined();
   });
 });
